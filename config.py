7de5281d78be396f358ce1599c279d3f4c5ae725
Pavel Pavlyuchik <xenearz@gmail.com>
2025-08-05 16:08:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 FRAME_CONFIGS = [
     {
         "file_id": "7V4UQ61IVRxGArYZ20n7MH",  # Из URL App Components
-        "node_id": "18723:231737",            # Из URL после node-id=
+        "node_id": "18723:231737",
         "title": "App Components"
     },
     {
@@ -13,5 +13,35 @@ FRAME_CONFIGS = [
         "file_id": "oZGlxnWyOHTAgG6cyLkNJh",  # Web Components Molecules 2.0
         "node_id": "145226:118013",
         "title": "Web Components Molecules 2.0"
+    },
+    {
+        "file_id": "iqTCxAPRJJm6UlANLlYfva",  # Variables
+        "node_id": "19090:1487",
+        "title": "Variables"
+    },
+    {
+        "file_id": "pFdOFGWgb8OPunqMJ8D3Ia",  # Ecosystem Modules
+        "node_id": "88:451",
+        "title": "Ecosystem Modules"
+    },
+    {
+        "file_id": "naQ5PQP9TK0PGJN424Vynl",  # Base
+        "node_id": "19090:1487",
+        "title": "Base"
+    },
+    {
+        "file_id": "a3nZmvTc8B9cZcrke9goCE",  # Icons
+        "node_id": "51290:2217",
+        "title": "Icons"
+    },
+    {
+        "file_id": "yqrOXR0gW6mm76ZocspYQP",  # Placeholders
+        "node_id": "88:451",
+        "title": "Placeholders"
+    },
+    {
+        "file_id": "pd0yLOVBwTBATyqsDYZegP",  # Graphic Assets
+        "node_id": "51290:2217",
+        "title": "Graphic Assets"
     }
 ]
